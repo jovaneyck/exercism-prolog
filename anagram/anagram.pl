8bd842f4a916907ca5e124884b2aaffbf19b8c90
Jo Van Eyck <jo.vaneyck@ae.be>
2018-10-12 16:14:08 +0200

    anagrams

--- anagram/anagram.pl
+++ anagram/anagram.pl
@@ -1,1 +1,24 @@
-anagram(Word, Options, Matching).
+anagram(_, [], []) :- !.
+anagram(Word, [O | Opts], [O | RMatching]) :-
+    is_anagram(Word, O),
+    anagram(Word, Opts, RMatching),
+    !.
+anagram(Word, [ _ | Opts], RMatching) :-
+    anagram(Word, Opts, RMatching).
+
+is_anagram(One,Other) :-
+    string_lower(One, OneLower),
+    string_lower(Other, OtherLower),
+    OneLower \= OtherLower,
+    string_chars(OneLower, Ones),
+    string_chars(OtherLower, Others),
+    is_permutation(Ones, Others).
+
+is_permutation([],[]).
+is_permutation([H|T], Other) :-
+    without(H, Other, OtherWithout),
+    is_permutation(T, OtherWithout).
+
+without(X, [X|T], T).
+without(X, [H|T], [H|W]) :-
+    without(X, T, W).
